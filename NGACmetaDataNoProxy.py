bbfb655d9804225e732635262a070f4004fde2aa
tengwei <471435549@qq.com>
2017-03-04 12:41:31 +0800

    添加了NGACmetaDataNoProxy.py， 修改了NGACmetaData.py,为其添加了分文件夹保存文件的功能

--- NGACmetaDataNoProxy.py
+++ NGACmetaDataNoProxy.py
@@ -7,17 +7,10 @@ import traceback
 import MySQLdb
 import urllib
 import time
-import threading
+import random
 import os
-# from UsefulProxyPool import runningPool
-# import threadpool
-from mainProcess import *
-import time
-from requests.exceptions import ProxyError
-from NGACSPiderExceptions import ProxyPoolDBEmptyException
-
-lock = threading.Lock()
-
+from UsefulProxyPool import runningPool
+from requests.exceptions import ProxyError, Timeout, ConnectionError
 
 
 class metadataDbOps():
@@ -34,9 +27,12 @@ class metadataDbOps():
 
         pass
 
-    def transactionsTemplate(self, sql, succStr='', failedStr=''):
+    def transactionsTemplate(self, sql, succStr='', failedStr='', executemany=False):
         try:
-            self.cursor.execute(sql)
+            if executemany:
+                self.cursor.executemany(sql)
+            else:
+                self.cursor.execute(sql)
             self.db.commit()
             print succStr
             return True
@@ -51,7 +47,7 @@ class metadataDbOps():
         sql = """
         create table ngacMetaData(
         mdIdnt varchar(220),
-        metadataState enum("undownload","downloading", "downloaded") default "undownload",
+        metadataState enum("undownload", "downloading","downloaded") default "undownload",
         primary key(mdIdnt)
         )default charset=utf8;
         """
@@ -67,14 +63,10 @@ class metadataDbOps():
 
     def iFinishedDownload(self):
         """ 下载完返回False, 否则返回新的id """
-        sql = "select mdIdnt from ngacMetaData where metadataState = 1 limit 1;"
-        sql2 = "update ngacMetaData set metadataState = 2 where mdIdnt = '%s'"
+        sql = "select mdIdnt from ngacMetaData where metadataState = 1 limit 1 for update;"
         try:
             self.cursor.execute(sql)
             tmplist = self.cursor.fetchone()
-            sql2 = sql2 % tmplist[0]
-            self.cursor.execute(sql2)
-            self.db.commit()
             print tmplist
             if tmplist == None:  # 待下载列表为空：下载完
                 print "下载完成！"
@@ -83,8 +75,7 @@ class metadataDbOps():
                 print "获得新id"
                 return tmplist[0]
         except Exception:
-            self.db.rollback()
-            print "查询或修改下载状态信息出错！"
+            print "查询下载状态信息出错！"
             print traceback.format_exc()
         pass
     #
@@ -101,34 +92,25 @@ class metadataDbOps():
 
 class proxyPool2():
     def __init__(self, Host, User, passwd, databaseName):
-        # self.tp = runningPool(Host, User, passwd, databaseName, timeout=20)
-        # self.tp.run()
+        self.tp = runningPool(Host, User, passwd, databaseName, timeout=20)
+        # self.tp.run(timeRange=1440)
         # self.pool = self.tp.pool
-        self.Timer_p = Timer_ProxyPool(Host, User, passwd, databaseName, timeout=20)
-
+        # print self.pool
+        self.pool = set([u'113.233.153.114:8118', u'219.148.108.126:8998', u'125.118.77.48:808', u'171.38.196.230:8123', u'111.76.133.91:808', u'111.76.133.198:808', u'60.13.74.211:80', u'111.76.129.38:808', u'106.46.136.58:808', u'115.28.134.77:80', u'111.76.129.103:808', u'111.76.129.106:808', u'111.76.129.186:808', u'119.162.51.22:8118', u'106.46.136.8:808', u'114.215.80.237:80', u'120.25.123.95:80', u'171.39.26.116:8123', u'222.134.134.250:8118', u'203.90.144.145:80', u'111.76.129.122:808'])
         pass
 
     def updateProxyPool(self):
-        #self.tp.run()
-        #self.pool = self.tp.pool
-        self.Timer_p.startProxyPool()
-
+        self.tp.run(multiNum=30)
+        self.pool = self.tp.pool
+        print self.pool
 
     def getoneIp(self):
-        """
         if len(self.pool) == 0:
             self.updateProxyPool()
         return self.pool.pop()
-        """
-        tmpIp = self.Timer_p.getOneProxyIP()
-        print tmpIp
-        if tmpIp == False:
-            raise ProxyPoolDBEmptyException
-            return False
-        return tmpIp
         pass
 
-class getmetaDatafromNGAC(metadataDbOps, threading.Thread):
+class getmetaDatafromNGAC(metadataDbOps):
     headers = {
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
         # "Accept-Encoding": "gzip, deflate",
@@ -137,56 +119,71 @@ class getmetaDatafromNGAC(metadataDbOps, threading.Thread):
         "Connection": "keep-alive",
         "DNT": '1',
         "Host": "catalog.ngac.org.cn",
-        "Host": "catalog.ngac.org.cn",
         # "Referer": """http://mail.qq.com/cgi-bin/mail_spam?action=check_link&url=http://catalog.ngac.org.cn/mutualsearch-access/csw?service=CSW%26request=GetRecordById%26version=2.0.2%26ElementSetName=full%26ID=cgdoi.n0001/x00063534.t02_0033&mailid=GAcJb3MGAAQOGQVbQEJxX1JoAFBNdGxLCX13dHZ+UwMB&spam=0""",
         "Upgrade-Insecure-Requests": '1',
         "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:50.0) Gecko/20100101 Firefox/50.0",
     }
-
-    def __init__(self, Host, User, passwd, databaseName, prefixpath="/home/wlw/Documents/gitSpider/data"):
+    
+    
+    def __init__(self, Host, User, passwd, databaseName):
         metadataDbOps.__init__(self, Host, User, passwd, databaseName)
-        self.pp2 = proxyPool2(Host, User, passwd, "ProxyPool2")
-	self.prefix = prefixpath
+        self.pp2 = proxyPool2("localhost", User, passwd, "ProxyPool2")
+        self.prefix = "/home/david/文档/data/NGACmetadata_"     # 存放文件的文件夹路径的前缀
+        self.dircount = 1                                       # 上述文件夹的序号：后缀
+        self.fullfileNums = 10000                               # 每个文件加放10000条数据
+        self.preventfileNums = 0                                # 上次中断前/当前文件夹中文件的个数
+        self.getdircount_preNums()                              # 更新dircount 和 preventfileNums 
+        self.loopperiod = 100
+        # self.getprefixpath()    # 包含更新数据
+
+        """
+        self.prefix = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))   # self.prefix = "/home/david/文档/data/"
+        self.prefix = os.path.dirname(self.prefix)        
+        self.prefix += "/data/"
+        """
         pass
 
-    def requestWithproxy(self, headers, data_encode, useProxy=True):
-        # proxies = {"http": 'http://' + self.pp2.getoneIp()} if useProxy else None
+    def getdircount_preNums(self):
+        while(self.getfilenums(self.getprefixpath()) >= self.fullfileNums):
+            self.dircount += 1
+            if os.path.isdir(self.getprefixpath()) == False:
+                os.makedirs(self.getprefixpath())
+        self.preventfileNums = self.getfilenums(self.getprefixpath())
+
+        #if os.path.isdir(self.getprefixpath()) == False:
+        #    os.makedirs(self.getprefixpath())
+        pass
+
+    def requestWithproxy(self, headers, data_encode, useProxy=True, newproxyIp=''):
+        if useProxy and newproxyIp == '':
+            newproxyIp = self.pp2.getoneIp()
+        proxies = {"http": 'http://' + newproxyIp} if useProxy else None
         try:
-            proxies = {"http": 'http://' + self.pp2.getoneIp()} if useProxy else None
             res = requests.get("http://catalog.ngac.org.cn/mutualsearch-access/csw?", headers=self.headers, params=data_encode, timeout=30, proxies=proxies)
-            return True, res
+            return True, res, newproxyIp
         except ProxyError:
             print "ProxyError, change proxy Ip and try again..."
-            return False,None
-        except ProxyPoolDBEmptyException as e:
-            print e.Message
-            return -1, None
-        except Exception as e:    # ReadTimeout: HTTPConnectionPool
-            print e
-            print "未知异常..."
-            return -2, None
+            return False, None, ''
+        except Timeout:
+            print "Proxy request Timeout, change proxy Ip and try again..."
+            return False, None, ''
+        except ConnectionError:
+            print "Connection Error, change proxy Ip and try again..."
+            return False, None, ''
+        except Exception:
+            print "other Error, change proxy Ip and try again..."
+            return False, None, ''
         pass
 
-    def getdirfilesNum(self, dirpath):
-	if os.path.isdir(dirpath):
-	    return len(os.listdir(dirpath))
-	else:
-	    print "this is not a dir path..."
-            return -1	    
-	pass
-
     def getprefixpath(self):
-	prepathlist = os.listdir(self.prefix)
-        maxdirnum = max([x.split('_')[1] for x in prepathlist if '_' in x])
-	tmpdirfilesnum = self.getdirfilesNum(self.prefix + "/Ngacmetadata_" + str(maxdirnum))
-	if tmpdirfilesnum >= 10000:
-	    maxdirnum += 1
-	    os.mkdir(self.prefix + "/Ngacmetadata_" + str(maxdirnum))
-	return 	self.prefix + "/Ngacmetadata_" + str(maxdirnum) + "/"
-	pass
+        return self.prefix + str(self.dircount) + "/"
+
+    
+    def getfilenums(self, dirpath):
+        return len([x for x in os.listdir(dirpath)])
 
 
-    def getmetaData(self, mdIdnt, useProxy=False):
+    def getmetaData(self, mdIdnt, prefixPath="/home/david/文档/data/NGACmetadata_1/", useProxy=False):
         data = {
             "ElementSetName": "full",
             "ID": mdIdnt,
@@ -195,94 +192,61 @@ class getmetaDatafromNGAC(metadataDbOps, threading.Thread):
             "version": "2.0.2",
         }
         data_encode = urllib.urlencode(data)
-        flag, res = self.requestWithproxy(headers=self.headers, data_encode=data_encode,useProxy=True)
-        while(flag in (False, -1,-2)):
-            if flag == -1:
-                time.sleep(30)
-            flag, res = self.requestWithproxy(headers=self.headers, data_encode=data_encode,useProxy=True)
-                
-        #proxies = {"http": 'http://' + self.pp2.getoneIp()} if useProxy else None
+        flag, res, oldproxyIP= self.requestWithproxy(headers=self.headers, data_encode=data_encode, useProxy=useProxy)
+        while(flag == False):
+            flag, res, oldproxyIP = self.requestWithproxy(headers=self.headers, data_encode=data_encode, useProxy=useProxy, newproxyIp=oldproxyIP)
+        # proxies = {"http": 'http://' + self.pp2.getoneIp()} if useProxy else None
         # res = requests.get("http://catalog.ngac.org.cn/mutualsearch-access/csw?", headers=self.headers, params=data_encode, timeout=30, proxies=proxies)
         mdIdnt = mdIdnt.replace('/', '+')
         print res.status_code
         # print res.headers
         # print res.content
         # print res.content
-	lock.acquire()
-	prefixPath = self.getprefixpath()
-        with open(prefixPath + str(mdIdnt) + ".xml", "w") as wr:
+        with open(prefixPath + mdIdnt + ".xml", "w") as wr:
             wr.write(res.content)
-	lock.release()
+
             # tmpobj = getDocsByPost()
             # print tmpobj.getDocsInfo()
         pass
 
     def run(self):
-        newid = self.iFinishedDownload()
+        newid = self.iFinishedDownload() 
+
         while(newid):
             try:
-                self.getmetaData(newid, useProxy=True)
+                self.getmetaData(newid, self.getprefixpath(), useProxy=False)
             except Exception:
                 print "获取数据失败..."
                 print traceback.format_exc()
                 return False
 
             if self.changestate(newid) == False:
-                return False        
-            
+                return False
+
             newid = self.iFinishedDownload()
 
-        pass
+            self.preventfileNums += 1
+            if  self.preventfileNums > self.fullfileNums:
+                self.dircount += 1
+                self.preventfileNums = 1
+                if os.path.isdir(self.getprefixpath()) == False:
+                    os.makedirs(self.getprefixpath())
+
+            if self.preventfileNums%self.loopperiod == 0:
+                time.sleep(random.randint(1, 20))
 
     def parsexmlstring(self):  # xml.dom.minidom.parseString
 
         pass
-class multiProcesofgettingmetadata():
-    def __init__(self, Host, User, passwd, databaseName, multiprcNum=2):
-        self.multiprcNum = multiprcNum
-        """
-        baseSrgList = (Host, User, Passwd, databaseName,)
-        self.argslist = []
-        for _ in range(multiprcNum):
-            self.argslist.append(baseSrgList)
-        """
-        self.tmpobj = getmetaDatafromNGAC(Host, User, passwd, databaseName)
-
-    def startmultiprocess(self):
-        """
-        pool = threadpool.ThreadPool(self.multiprcNum)
-        requests = threadpool.makeRequests(tmpobj.run())
-        [pool.putRequest(req) for req in requests]
-        pool.wait()
-        """
-        ThreadList = []
-        for _ in range(self.multiprcNum):
-            ThreadList.append(self.tmpobj.run())
-         
-        for i in range(self.multiprcNum):
-            ThreadList[i].start()
-
-        for i in range(self.multiprcNum):
-            ThreadList[i].join()
-
-        pass
-    pass
 
 if __name__ == "__main__":
     # ********************metadataDbOps类的测试和数据库表的初始化*************************
-    # db = metadataDbOps("localhost", "root", "tw1120150633", "NGACDOCSINFO")
+    # db = metadataDbOps("localhost", "root", "tw2016941017", "NGACDOCSINFO")
     # db.createCtrTable()       # 创建控制表格
     # db.initCtrTable()         # 初始化控制表格
     # db.iFinishedDownload()  # 测试状态查询
 
     # ********************运行测试*********************************************************
-    """
-    tmpobj = getmetaDatafromNGAC("localhost", "root", "tw1120150633", "NGACDOCSINFO")
+    tmpobj = getmetaDatafromNGAC("localhost", "root", "tw2016941017", "NGACDOCSINFO")
     tmpobj.run()
-    """
-    #tmpobj.iFinishedDownload()
-
-    # ****************多线程（线程池）测试*************************************************
-    tmpobj = multiProcesofgettingmetadata("localhost", "root", "tw1120150633", "NGACDOCSINFO")
-    tmpobj.startmultiprocess()
-
+    pass
